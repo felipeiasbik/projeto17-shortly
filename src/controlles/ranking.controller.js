956d9affde781342d7891b7c10abf08eb4ee2736
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-05-21 13:51:04 -0300

    presentation: add repository pattern

--- src/controlles/ranking.controller.js
+++ src/controlles/ranking.controller.js
@@ -1,24 +1,8 @@
-import { db } from "../database/database.connection.js";
+import { getRankingDB } from "../repositories/ranking.repository.js";
 
 export async function getRanking(req, res){
     try {
-        const ranking = await db.query(`
-        SELECT
-            u.id,
-            u.name,
-            CAST(COUNT(DISTINCT us.id) AS INTEGER) AS "linksCount",
-            COALESCE(SUM(uc."visitCount"),0)::INTEGER AS "visitCount"
-        FROM
-            users u
-            LEFT JOIN session s ON u.id = s."userId"
-            LEFT JOIN url ur ON u.id = ur."userId"
-            LEFT JOIN "urlShorten" us ON ur.id = us."urlId"
-            LEFT JOIN "urlClicks" uc ON us.id = uc."urlShortenId"
-        GROUP BY
-            u.id
-        ORDER BY
-            "visitCount" DESC
-        LIMIT 10;`);
+        const ranking = await getRankingDB();
         res.send(ranking.rows);
     } catch (err) {
         res.status(500).send(err.message);
---
+++ src/repositories/users.repository.js
@@ -0,0 +1,41 @@
+import { db } from "../database/database.connection.js";
+
+export function getUserIdDB(token){
+    const result = db.query(`SELECT * FROM session WHERE token=$1`, [token]);
+    return result;
+}
+
+export function getUserFirstDB(id){
+    const result = db.query(`
+    SELECT
+        u.id,
+        u.name,	
+        COALESCE(SUM(uc."visitCount"),0) AS "visitCount"         
+    FROM users u
+    JOIN session s ON u.id = s."userId"
+    JOIN url ur ON u.id = ur."userId" 
+    JOIN "urlShorten" us ON ur.id = us."urlId"
+    JOIN "urlClicks" uc ON us.id = uc."urlShortenId"
+    WHERE u.id = $1
+    GROUP BY u.id;
+    `, [id.rows[0].userId]);
+    return result;
+}
+
+export function getUserSecondDB(id){
+    const result = db.query(`
+    SELECT 
+        us.id, 
+        us."urlShorten", 
+        ur.url, 
+        COALESCE(SUM(uc."visitCount"),0) AS "visitCount"   
+    FROM users u
+    JOIN session s ON u.id = s."userId"
+    JOIN url ur ON u.id = ur."userId" 
+    JOIN "urlShorten" us ON ur.id = us."urlId"
+    JOIN "urlClicks" uc ON us.id = uc."urlShortenId"
+    WHERE u.id = $1
+    GROUP BY us.id, ur.id;
+    `, [id.rows[0].userId]);
+    return result;
+}
